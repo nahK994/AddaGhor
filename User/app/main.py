7d0af6bdd23398d63259b5ac2612f8cc802bf6f9
nahK994 <nkskl6@gmail.com>
2022-03-05 13:50:43 +0600

    update User

--- User/app/main.py
+++ User/app/main.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Response
 from typing import Optional, List
 
 app = FastAPI()
 
-class UserInfo(BaseModel):
+class UserModel(BaseModel):
+    userId: Optional[str]
     userName: str
     bio: Optional[str] = None
     email: str
 
-class UserModel(BaseModel):
-    userId: str
+class CreateUserModel(BaseModel):
     userName: str
     bio: Optional[str] = None
     email: str
@@ -21,14 +21,14 @@ userList: List[UserModel] = []
 def getUser(user_id: int):    
     for user in userList:
         if user.userId == str(user_id):
-            userInfo = UserInfo(
+            userModel = CreateUserModel(
                 userName = user.userName,
                 bio = user.bio,
                 email = user.email
             )
-            return userInfo
+            return userModel
 
-    return "User not found"
+    raise HTTPException(status_code=404, detail="User not found")
 
 @app.get("/users")
 def getUsersList():    
@@ -40,12 +40,12 @@ def deleteUser(user_id: int):
     for user in userList:
         if str(user_id) == user.userId:
             userList.remove(user)
-            return "deleted"
+            return Response(status_code=204)
 
-    return "can not be deleted"
+    raise HTTPException(status_code=500, detail="Cannot be deleted")
 
 @app.put("/user/update/{user_id}")
-def updateUser(userInfo: UserInfo, user_id: int):    
+def updateUser(userInfo: UserModel, user_id: int):    
     
     for user in userList:
         if str(user_id) == user.userId:
@@ -54,12 +54,12 @@ def updateUser(userInfo: UserInfo, user_id: int):
             user.email = userInfo.email
             return "updated"
 
-    return "can not be updated"
+    raise HTTPException(status_code=500, detail="Cannot update")
 
 @app.post("/user/create")
-def getUsersList(userInfo: UserInfo):    
+def getUsersList(userInfo: CreateUserModel):  
     if(isEmailUnique(userInfo.email) == False):
-        return "Email has been used"
+        raise HTTPException(status_code=400, detail="Email has been used")
 
     userData = UserModel(
         userName = userInfo.userName,
