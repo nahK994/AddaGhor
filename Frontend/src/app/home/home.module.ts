25b579ebe2fad21294c08560288933b355fc846f
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2022-03-23 03:49:17 +0600

    ui of landing page navbar

--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -4,15 +4,17 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomeRoutingModule } from './home-routing-module';
 import { HomeService } from './home.service';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
     HomeService
