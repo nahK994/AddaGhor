90f2efe6df938d16760b4cfee8ae0a72581ff4e8
nahK994 <nkskl6@gmail.com>
2022-03-27 16:14:14 +0600

    complete post, comment update

--- Frontend/src/app/shared/post-card/post-card.component.ts
+++ Frontend/src/app/shared/post-card/post-card.component.ts
@@ -1,6 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { CreatePost, CreatePostComment, Timeline } from 'src/app/home/home.interface';
+import { CreatePost, CreatePostComment, PostComment, Timeline } from 'src/app/home/home.interface';
 
 export interface CommentEvent {
   postId: number;
@@ -32,7 +33,7 @@ export class PostCardComponent implements OnInit {
     this.timeLineInfo = val;
   }
 
-  @Input('userId') userId: string;
+  @Input('userId') userId: number;
 
   @Output() likeEvent: EventEmitter<number> = new EventEmitter();
   @Output() smileEvent: EventEmitter<number> = new EventEmitter();
@@ -40,10 +41,17 @@ export class PostCardComponent implements OnInit {
 
   @Output() commentEvent: EventEmitter<CommentEvent> = new EventEmitter();
 
-  @Output() updatePost: EventEmitter<CreatePost> = new EventEmitter();
-  @Output() updateComment: EventEmitter<CreatePostComment> = new EventEmitter();
+  @Output() updatePost: EventEmitter<UpdatePostOutput> = new EventEmitter();
+  @Output() updateComment: EventEmitter<UpdateCommentOutput> = new EventEmitter();
 
   newComment = new FormControl();
+
+  post: FormControl = new FormControl();
+  comment: FormControl = new FormControl();
+
+  isPostEditMode: boolean;
+  isCommentEditMode: boolean;
+  commentInfoToUpdate: PostComment;
 
   constructor() { }
 
@@ -70,11 +78,41 @@ export class PostCardComponent implements OnInit {
   }
 
   editPost() {
-
+    this.post.setValue(this.timeLineInfo.postText);
+    this.isPostEditMode = true;
   }
 
-  editComment() {
+  submitPost() {
+    this.isPostEditMode = false;
+    this.updatePost.emit({
+      postId: this.timeLineInfo.postId,
+      postInfo: {
+        userId: this.userId,
+        userName: this.timeLineInfo.userName,
+        postText: this.post.value,
+        postDateTime: this.timeLineInfo.postDateTime
+      }
+    })
+  }
 
+  editComment(comment: PostComment) {
+    this.comment.setValue(comment.commentText);
+    this.commentInfoToUpdate = comment
+    this.isCommentEditMode = true;
+  }
+
+  submitComment() {
+    this.isCommentEditMode = false;
+    this.updateComment.emit({
+      commentId: this.commentInfoToUpdate.commentId,
+      commentInfo: {
+        postId: this.timeLineInfo.postId,
+        userId: this.userId,
+        commentDateTime: this.commentInfoToUpdate.commentDateTime,
+        commentText: this.comment.value,
+        userName: this.commentInfoToUpdate.userName
+      }
+    })
   }
 
 }
