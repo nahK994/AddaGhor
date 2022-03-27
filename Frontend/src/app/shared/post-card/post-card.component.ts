7cafd19ce7cf41b9dd175495144ca7d63b1e9c32
nahK994 <nkskl6@gmail.com>
2022-03-27 09:12:38 +0600

    initiate post-card module

--- Frontend/src/app/shared/post-card/post-card.component.ts
+++ Frontend/src/app/shared/post-card/post-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CreatePostComment, Timeline } from 'src/app/home/home.interface';
 
 @Component({
   selector: 'app-post-card',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCardComponent implements OnInit {
 
+  timeLineInfo: Timeline;
+  @Input() set setTimeLineInfo(val: Timeline) {
+    if(!val) {
+      return;
+    }
+    this.timeLineInfo = val;
+  }
+  @Output() likeEvent: EventEmitter<number> = new EventEmitter();
+  @Output() smileEvent: EventEmitter<number> = new EventEmitter();
+  @Output() loveEvent: EventEmitter<number> = new EventEmitter();
+
+  @Output() commentEvent: EventEmitter<CreatePostComment> = new EventEmitter();
+
+  newComment = new FormControl();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  likeReact() {
+    this.likeEvent.emit(this.timeLineInfo.postId);
+  }
+
+  smileReact() {
+    this.smileEvent.emit(this.timeLineInfo.postId);
+  }
+
+  loveReact() {
+    this.loveEvent.emit(this.timeLineInfo.postId);
+  }
+
+  commentPost() {
+    this.commentEvent.emit({
+      postId: this.timeLineInfo.postId,
+      userId: this.timeLineInfo.userId,
+      userName: this.timeLineInfo.userName,
+      commentText: this.newComment.value,
+      commentDateTime: (new Date()).toDateString()
+    })
   }
 
 }
--- Frontend/src/app/shared/post-card/post-card.module.ts
+++ Frontend/src/app/shared/post-card/post-card.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostCardComponent } from './post-card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularEmojisModule } from 'angular-emojis';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [PostCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    AngularEmojisModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class PostCardModule { }
