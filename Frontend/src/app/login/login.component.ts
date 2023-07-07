36a6261e82dca12fe17cd6bcbe4f8532b376c583
nahK994 <nkskl6@gmail.com>
2023-07-07 23:55:58 +0600

    update login page

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AppService } from '../app.service';
+import { Router } from '@angular/router';
+// import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +14,7 @@ export class LoginComponent {
   constructor(
     private _router: Router,
     private _formBuilder: FormBuilder,
-    private _activatedRoute: ActivatedRoute,
-    private _appService: AppService
+    // private _appService: AppService
   ) {
     this.loginForm = this._formBuilder.group({
       email: [''],
@@ -27,20 +26,20 @@ export class LoginComponent {
     this._router.navigate(['user/create'])
   }
 
-  async onSubmit(){
-    try {
-      let userId = await this._appService.loginUser({
-        email: this.loginForm.get('email').value,
-        password: this.loginForm.get('password').value
-      })
+  async login(){
+    // try {
+    //   let userId = await this._appService.loginUser({
+    //     email: this.loginForm.get('email').value,
+    //     password: this.loginForm.get('password').value
+    //   })
   
-      this._router.navigate(['..', 'home', userId], {
-        relativeTo: this._activatedRoute
-      })
-    }
-    catch(error) {
-      console.log("haha ==> ", error)
-    }
+    //   this._router.navigate(['home', userId])
+    // }
+    // catch(error) {
+    //   console.log("haha ==> ", error)
+    // }
+
+    this._router.navigate(['home', 1])
   }
 
 }
