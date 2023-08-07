8e39e7baf695d6619ccdfd294aa8601529433118
nahK994 <nkskl6@gmail.com>
2023-08-07 21:37:21 +0600

    create comment model

--- Backend/timeline/models.py
+++ Backend/timeline/models.py
@@ -4,9 +4,19 @@ from user.models import User
 
 # Create your models here.
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     text = models.TextField()
     date = models.DateField(auto_now=True)
 
     class Meta:
         db_table = 'posts'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    text = models.TextField()
+    date = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'comments'
