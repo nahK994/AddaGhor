cd4573bc7e0e681774dd66d4a3ee5964438028f3
nahK994 <nkskl6@gmail.com>
2023-08-09 22:06:10 +0600

    create comment command serializer

--- Backend/timeline/serializers.py
+++ Backend/timeline/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -28,6 +28,53 @@ class PostSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         return {
             "id": instance.id,
+            "text": instance.text,
+            "date": instance.date.strftime("%D")
+        }
+
+    
+class CommentCommandSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = []
+
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        if request.method == "POST":
+            post = Post.objects.filter(id=attrs['postId'])
+            if not len(post):
+                raise serializers.ValidationError("No such post")
+        return attrs
+
+    
+    def create(self, validated_data):
+        request = self.context.get('request')
+        data = validated_data
+        post = Post.objects.filter(id=validated_data['postId'])[0]
+        comment_obj = Comment.objects.create(
+            user = request.user,
+            text = data['text'],
+            post = post
+        )
+        return comment_obj
+
+
+    def update(self, instance, validated_data):
+        if instance is None:
+            raise serializers.ValidationError("No such item")
+
+
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+        return instance
+    
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "postId": instance.post.id,
             "text": instance.text,
             "date": instance.date.strftime("%D")
         }
