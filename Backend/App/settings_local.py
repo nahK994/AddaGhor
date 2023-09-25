e5bb1eff2e00db3c73ae7d49fda8dfdae1ed16df
nahK994 <nkskl6@gmail.com>
2023-09-25 22:38:15 +0600

    remove some unnecessary codes

--- Backend/App/settings_local.py
+++ Backend/App/settings_local.py
@@ -1,17 +1,13 @@
 from .settings import *
-from dotenv import load_dotenv
-load_dotenv(dotenv_path='../.env')
-import os
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'adda_ghor'),
-        'USER': os.getenv('POSTGRES_USER', 'skhan'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'haha'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'adda_ghor',
+        'USER': 'skhan',
+        'PASSWORD': 'haha',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
