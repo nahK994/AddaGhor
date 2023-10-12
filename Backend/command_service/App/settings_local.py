1dbcb4d095993ee6957e486c904104c8dad9ee1c
nahK994 <nkskl6@gmail.com>
2023-10-13 02:29:19 +0600

    conbine all things up

--- Backend/command_service/App/settings_local.py
+++ Backend/command_service/App/settings_local.py
@@ -4,10 +4,10 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'command_db_adda_ghor'),
-        'USER': os.getenv('POSTGRES_USER', 'skhan'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'haha'),
-        'HOST': '0.0.0.0',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'command_db',
         'PORT': '5000',
     }
 }
