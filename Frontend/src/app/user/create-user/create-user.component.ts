3fa05b08a456520e626831a90a491e23852aeb72
nahK994 <nkskl6@gmail.com>
2023-10-20 13:59:12 +0600

    add email confirmation snackBar in registration

--- Frontend/src/app/user/create-user/create-user.component.ts
+++ Frontend/src/app/user/create-user/create-user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User, UserService } from '../user.service';
 import { AppService } from 'src/app/app.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-user',
@@ -13,13 +14,15 @@ export class CreateUserComponent {
   constructor(
     private _userService: UserService,
     private _router: Router,
-    private _appService: AppService
+    private _appService: AppService,
+    private _snackBar: MatSnackBar
   ) { }
 
   async signUp(userInfo: User) {
     this._appService.removeAssets();
     try {
       await this._userService.createUser(userInfo);
+      this._snackBar.open('A confirmation email has been sent to email', 'close');
       this._router.navigate([''])
     }
     catch (error) {
